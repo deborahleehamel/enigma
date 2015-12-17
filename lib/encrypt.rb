bce7ebeb3379e9d90a817a582e2fd7b24ced52a9
Deborah Hamel <deborahleehamel.gmail>
2015-12-17 06:57:58 -0700

    Comment out seeing is beliefing text.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,20 +1,13 @@
-require_relative 'cypher'   # => true
-require_relative 'engine'   # => true
-require_relative 'rotator'  # => true
-require 'pry'               # => false
+require_relative 'cypher'
+require_relative 'engine'
+require_relative 'rotator'
+require 'pry'
 
 class Encrypt
   def encrypt(message)
     c = Rotator.new(message,Cypher.new.character_map).rotated
     puts c
-  end                                                          # => :encrypt
-end                                                            # => :encrypt
+  end
+end
 
-#Encrypt.new("Hello World")  # ~> ArgumentError: wrong number of arguments (1 for 0)
-
-# ~> ArgumentError
-# ~> wrong number of arguments (1 for 0)
-# ~>
-# ~> /Users/deborahhamel/Turing/1module/my_project_folder/enigma/lib/encrypt.rb:14:in `initialize'
-# ~> /Users/deborahhamel/Turing/1module/my_project_folder/enigma/lib/encrypt.rb:14:in `new'
-# ~> /Users/deborahhamel/Turing/1module/my_project_folder/enigma/lib/encrypt.rb:14:in `<main>'
+#Encrypt.new("Hello World")
